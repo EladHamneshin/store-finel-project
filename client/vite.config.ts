fa2d9605485b458690bf8baddb9e9c122cc29e10
moshe ilan <moisi6510@gmail.com>
2023-12-01 06:18:41 +0200

    try to fix env problem

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+const arrayOfKeys = [
+  "VITE_PAYPAL_CLIENT_ID",
+  "VITE_API_KEY_LOCATION",
+];
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const processEnv = {};
+  arrayOfKeys.forEach(key => processEnv[key] = env[key]);
+  return {
+    define: {
+      'process.env': processEnv
+    },
     plugins: [react()],
     server: {
       port: 5173,
@@ -17,13 +29,14 @@ export default defineConfig({
     },
     build: {
       rollupOptions: {
-          output:{
-              manualChunks(id) {
-                  if (id.includes('node_modules')) {
-                      return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                  }
-              }
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
           }
+        }
       }
+    }
   }
-  })
+})
